d3dacce8f9d17bebfb10269b1a3313860883e7c8
Andy <andy.b.saylor@gmail.com>
2023-08-23 00:53:11 -0400

    Finish functionality
    - Add a way to color the squares
    - Add a way to erase colored squares
    - Add a way to clear the grid
    - Add a slider that controls grid size

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const container = document.querySelector('.container');
-const gridSize = 10;
-createGrid(gridSize);
+const initialGridSize = 16;
+createGrid(initialGridSize);
 
 // Create nxn grid
 function createGrid(gridSize) {
+  // Reset when it gets updated
+  container.innerHTML = '';
   for (let i = 0; i < gridSize * gridSize; i++) {
     // Create the grid items within the container
     const item = document.createElement('div');
@@ -12,4 +14,64 @@ function createGrid(gridSize) {
     item.style.flex = `1 0 calc(100% / ${gridSize})`;
     container.appendChild(item);
   }
+}
+
+// Individual squares
+const items = document.querySelectorAll('.item');
+
+// For each square, color them by adding an extra class
+// with a different background color
+items.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    item.classList.add('hovered');
+  });
+});
+
+// Slider
+const slider = document.querySelector('.slider');
+const gridValue = document.querySelector('.grid-value');
+
+function updateGridSize() {
+  const gridSize = slider.value;
+  gridValue.textContent = `${gridSize} x ${gridSize}`;
+  createGrid(gridSize);
+}
+
+slider.addEventListener('input', updateGridSize);
+
+// Clear grid
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click', () => {
+  items.forEach((item) => {
+    item.classList.remove('hovered');
+  });
+});
+
+// Toggle eraser
+const eraser = document.querySelector('.eraser');
+// Track state of eraser
+let isEraserToggled = false;
+
+eraser.addEventListener('click', toggleEraser);
+
+// Toggle eraser state
+function toggleEraser() {
+  isEraserToggled = !isEraserToggled;
+
+  // If toggled, add the listener. Otherwise, remove it.
+  if (isEraserToggled) {
+    items.forEach((item) => {
+      item.addEventListener('mouseover', handleEraser);
+    });
+  } else {
+    items.forEach((item) => {
+      item.removeEventListener('mouseover', handleEraser);
+    });
+  }
+}
+
+// Remove the 'hovered' class from the target item
+function handleEraser(e) {
+  e.target.classList.remove('hovered');
 }
