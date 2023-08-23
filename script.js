3ef0fb117936af076b610f674e7d627b94c615bc
Andy <andy.b.saylor@gmail.com>
2023-08-22 22:35:53 -0400

    Refactor Grid
    - Use flex-wrap instead of two for-loops
    - Dynamically calculate the growth of squares using flex

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const container = document.querySelector('.container');
-const gridSize = 4;
+const gridSize = 10;
+createGrid(gridSize);
 
-// probably make function later
-// Create the nxn grid
-for (let i = 0; i < gridSize; i++) {
-  // Create the specified amount of col divs
-  const col = document.createElement('div');
-  col.classList.add('col');
-
-  for (let j = 0; j < gridSize; j++) {
-    // Create the specified amount of item divs
-    // and add them to the col
+// Create nxn grid
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    // Create the grid items within the container
     const item = document.createElement('div');
     item.classList.add('item');
-    col.appendChild(item);
+    // Add dynamic growth according to grid size
+    item.style.flex = `1 0 calc(100% / ${gridSize})`;
+    container.appendChild(item);
   }
-
-  // After filling the col, add the col to the overall container
-  container.appendChild(col);
 }
